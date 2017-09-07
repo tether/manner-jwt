b72031c9fd937d1a21bf0e6b89dcaae0107389c9
bredele <olivier.wietrich@gmail.com>
2017-09-07 15:29:23 -0600

    Add jsonwebtoken dependency

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+/**
+ * Dependencies
+ */
+
+const manner = require('manner')
+const jwt = require('jsonwebtoken')
 
 /**
  * This is a simple description.
  *
+ * @param {String} path
+ * @param {String?} secret
  * @api public
  */
 
-module.exports = function () {
-  // do something
+module.exports = function (path, secret = process.env.JWT_SECRET) {
+  const service = manner(path)
+  return (req, res) => {
+    const token = ''
+    const obj = jwt.verify(token, secret)
+    if (obj) return service(req, res)
+  }
 }
-
-  
